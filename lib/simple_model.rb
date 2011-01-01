4dee54cdef31a3bc20ddad038280f26f64926a86
Joshua T. Mckinney <joshmckin@gmail.com>
2011-01-01 11:52:50 -0600

    require error and validation modules even if active_model is present

--- lib/simple_model.rb
+++ lib/simple_model.rb
@@ -2,11 +2,12 @@ module SimpleModel
 
   autoload :ExtendCore, "simple_model/extend_core"
   autoload :Attributes, "simple_model/attributes"
+  autoload :Errors, "simple_model/errors"
+  autoload :Validation, "simple_model/validation"
   begin
     require 'active_model'
   rescue LoadError
-    autoload :Errors, "simple_model/errors"
-    autoload :Validation, "simple_model/validation"
+    #Don't fail
   end
 
   class Base
