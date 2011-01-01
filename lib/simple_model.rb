0306339cbb20c221b9094588a0bca5d5d6812c2f
Joshua T. Mckinney <joshmckin@gmail.com>
2011-01-01 11:13:51 -0600

    integrate active_model

--- lib/simple_model.rb
+++ lib/simple_model.rb
@@ -2,12 +2,22 @@ module SimpleModel
 
   autoload :ExtendCore, "simple_model/extend_core"
   autoload :Attributes, "simple_model/attributes"
-  autoload :Errors, "simple_model/errors"
-  autoload :Validation, "simple_model/validation"
+  begin
+    require 'active_model'
+  rescue LoadError
+    autoload :Errors, "simple_model/errors"
+    autoload :Validation, "simple_model/validation"
+  end
 
   class Base
-    include SimpleModel::Errors
     include SimpleModel::Attributes
-    include SimpleModel::Validation   
+    begin
+      include ActiveModel::Validations
+      include ActiveModel::Conversion
+      extend ActiveModel::Naming
+    rescue NameError
+      include SimpleModel::Errors
+      include SimpleModel::Validation
+    end    
   end
 end
