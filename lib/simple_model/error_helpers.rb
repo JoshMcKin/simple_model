b72c2a4a20d4d8b174c2978c68a11edda8916eea
Joshua T. Mckinney <joshmckin@gmail.com>
2011-01-02 19:59:48 -0600

    wrapp error_helper module with simple_model

--- lib/simple_model/error_helpers.rb
+++ lib/simple_model/error_helpers.rb
@@ -1,26 +1,25 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+module SimpleModel
+  module ErrorHelpers
 
-module ErrorHelpers
-
-  def errors?
-    !self.errors.nil? && !self.errors.empty?
-  end
-
-  def errors_for_flash
-    error_string = "<div id='smErrorExplanation'>#{self.errors.count} errors prevented saving.</div>"
-    self.errors.full_messages.each do |m|
-      error_string << "<div>#{m}</div>"
+    def errors?
+      !self.errors.nil? && !self.errors.empty?
     end
-    error_string
-  end
+
+    def errors_for_flash
+      error_string = "<div id='smErrorExplanation'>#{self.errors.count} errors prevented saving.</div>"
+      self.errors.full_messages.each do |m|
+        error_string << "<div>#{m}</div>"
+      end
+      error_string
+    end
   
-  def errors_to_s
-    error_string = ""
-    self.errors.full_messages.each do |m|
-      error_string << "#{m} "
+    def errors_to_s
+      error_string = ""
+      self.errors.full_messages.each do |m|
+        error_string << "#{m} "
+      end
+      error_string
     end
-    error_string
-  end
    
+  end
 end
