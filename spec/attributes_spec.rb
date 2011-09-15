bc3b008b897e36f5938904d9281af68274d2a4ae
Joshua Mckinney <joshmckin@gmail.com>
2011-09-15 10:26:01 -0500

    fix autotest, update bundle

--- spec/attributes_spec.rb
+++ spec/attributes_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper.rb')
 
 describe SimpleModel::Attributes do
   before(:all) do
