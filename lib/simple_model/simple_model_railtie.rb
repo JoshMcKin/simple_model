e3068c830a44b944c45514b8fc06a1e27ae01557
Joshua Mckinney <joshmckin@gmail.com>
2011-11-16 09:30:45 -0600

    only add ActiveRecord Railtie if AR is present

--- lib/simple_model/simple_model_railtie.rb
+++ lib/simple_model/simple_model_railtie.rb
@@ -1,11 +1,13 @@
 module SimpleModel
   class SimpleModelRailtie  < ::Rails::Railtie
     initializer "simple_model.extend_core" do
-       extend SimpleModel::ExtendCore
+      extend SimpleModel::ExtendCore
     end
-    initializer "simple_model.ar_error_helper" do
-      class ActiveRecord::Base
-        include SimpleModel::ErrorHelpers
+    if Object.const_defined?(:ActiveRecord)
+      initializer "simple_model.ar_error_helper" do
+        class ActiveRecord::Base
+          include SimpleModel::ErrorHelpers
+        end
       end
     end
   end
